09d6afcc1f8699657e4fab32ce3bea88ce82d64b
Muhamad Fahmi Rahmatullah <fahmi.rahmat07@gmail.com>
2022-12-11 21:34:21 +0700

    handle form already filled but want to go another route. So add route guards to make sure

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -24,6 +24,7 @@
               :updateSong="updateSong"
               :index="i"
               :removeSong="removeSong"
+              :updateUnsavedFlag="updateUnsavedFlag"
             />
           </div>
         </div>
@@ -44,6 +45,7 @@ export default {
   data() {
     return {
       songs: [],
+      unsavedFlag: false,
     };
   },
   async created() {
@@ -70,6 +72,19 @@ export default {
 
       this.songs.push(song);
     },
+    updateUnsavedFlag(value) {
+      this.unsavedFlag = value;
+    },
+  },
+  beforeRouteLeave(to, from, next) {
+    if (!this.unsavedFlag) {
+      next();
+    } else {
+      const leave = confirm(
+        "You have unsaved change. Are you sure you want to leave?"
+      );
+      next(leave);
+    }
   },
   // using ref cancel
   // beforeRouteLeave(to, from, next) {
